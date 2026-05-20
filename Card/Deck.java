0b5bba498abb692bfb8d2896e24f6db17511a4a6
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-05-20 23:01:04 +0100

    deck java

---
+++ Card/Deck.java
@@ -0,0 +1,24 @@
+package poker;
+
+import java.util.*;
+
+public class Deck {
+
+    private List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        return cards.remove(0);
+    }
+}
